68433269648aed6e836929ce46e50f6fb4f809bf
tinray <b03902072@ntu.edu.tw>
2015-08-05 10:33:44 +0800

    add select file function

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,12 @@
 'use strict';
 
+var model = {
+  nowPdfUrl: ""
+};
+
 var view = {
   showPdf: function (){
+    DEFAULT_URL = controller.getNowPdfUrl();
     webViewerLoad();
     //$("#content").hide();
     $("#outerContainer").show();
@@ -12,11 +17,22 @@ var view = {
   }    
 };
 
-var init = function(){
-  $("#outerContainer").hide();
-  $("#showPdf").click( view.showPdf );
-  $("#pdfViewExit").click( view.hidePdf );
-
+var controller = {
+  getNowPdfUrl: function(){
+    return model.nowPdfUrl;
+  },
+  updateNowPdfUrl: function(){
+    model.nowPdfUrl = $("#pdfUrl").val();
+  },
+  init: function(){
+    $("#outerContainer").hide();
+    $("#showPdf").click( view.showPdf );
+    $("#pdfViewExit").click( view.hidePdf );
+    $("#pdfUrl").change( controller.updateNowPdfUrl );
+    controller.updateNowPdfUrl();
+  }
 };
 
-$(document).ready( init );
+
+
+$(document).ready( controller.init );
